5c719dd5d61d1ff909373a5828868b780eda9084
SivvonYoon <sivvon0308@knu.ac.kr>
2025-06-19 19:48:08 +0900

    1.0.0.

---
+++ install.sh
@@ -0,0 +1,23 @@
+sudo apt-get install -y build-essential cmake unzip pkg-config
+sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev v4l-utils libxvidcore-dev libx264-dev libxine2-dev
+sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt-get install -y libgtk-3-dev
+sudo apt-get install -y mesa-utils libgl1-mesa-dri libgtkgl2.0-dev libgtkglext1-dev
+sudo apt-get install -y libatlas-base-dev gfortran libeigen3-dev
+sudo apt-get install -y python2.7-dev python3-dev python-numpy python3-numpy
+sudo apt-get install -y libglade2-0 libsigc++-2.0-0v5 libcairomm-1.0-1v5 libglibmm-2.4-1v5 libatkmm-1.6-1v5 libpangomm-1.4-1v5 libgtkmm-2.4-1v5 libpangomm-1.4-1v5 libgtkmm-2.4-1v5
+sudo apt-get install -y libatkmm-1.6-dev libcairomm-1.0-dev libglade2-dev libglademm-2.4-dev libglibmm-2.4-dev libgtkglextmm-x11-1.2-0v5 libgtkglextmm-x11-1.2-dev libgtkmm-2.4-dev libpangomm-1.4-dev libsigc++-2.0-dev libxml2-dev
+sudo apt-get install -y sharutils
+sudo apt-get install -y libglade2-0 libsigc++-2.0-0v5 libcairomm-1.0-1v5 libglibmm-2.4-1v5 libatkmm-1.6-1v5 libpangomm-1.4-1v5 libgtkmm-2.4-1v5 libpangomm-1.4-1v5 libgtkmm-2.4-1v5
+sudo apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install -y python-rosdep
+sudo apt-get install -y ros-melodic-cv-bridge ros-melodic-tf ros-melodic-message-filters ros-melodic-image-transport
+sudo apt-get install -y ros-melodic-imu-complementary-filter ros-melodic-robot-pose-ekf
+echo "export ParkingRamp3D_PATH=/home/$USER/ParkingRamp3D/" >> ~/.bashrc
+sudo apt-get install -y cmake
+sudo apt-get install -y libgoogle-glog-dev libgflags-dev
+sudo apt-get install -y libatlas-base-dev
+sudo apt-get install -y libeigen3-dev
+sudo apt-get install -y libsuitesparse-dev
+sudo apt-get install -y sharutils
